4440b061439db5f1d4c4bfafec76ad4a808342f6
shaurya-prakash <shaurya_prakash@asl.org>
2014-10-15 06:43:18 +0100

    Shaurya Averager

--- Averager.java
+++ Averager.java
@@ -7,10 +7,25 @@
 *
 */
 
+import java.util.Scanner;
+
 public class Averager {
 	
 	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
 
+      Scanner scnr = new Scanner(System.in);
+      double NumTotal = 0.0;
+      double NumEntered = 0.0;
+      System.out.println("Enter a percentage grade, enter -1 when finished");
+      double NumAdd = scnr.nextDouble();
+      while (NumAdd != -1) { // while addCalories does NOT have the value of -1
+         NumTotal = NumTotal + NumAdd;
+         NumEntered = NumEntered + 1;
+         System.out.println("Enter a percentage grade, enter -1 when finished");
+
+         NumAdd = scnr.nextDouble();
+       }
+       
+       System.out.println("Your grade average is " + NumTotal / NumEntered + " %");
+     }
 }
